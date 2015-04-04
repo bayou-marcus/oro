3415eef3d42771c7480e7f3cd0e68c57e225525c
Joel Wagener <bayou.marcus@gmail.com>
2015-04-04 01:53:23 -0400

    Added class components method.

--- lib/auguste/password.rb
+++ lib/auguste/password.rb
@@ -2,10 +2,11 @@ require_relative 'parts'
 require_relative 'settings'
 
 # FIXME This should be a functional and wholly self-contained class, where when you create an instance of it with no params, it gives you back a password (maybe based on random parts and lengths).  If you pass options, then it gives you one like you requested. That would mean all parts namespaced here, defaults separated from Preference to a new singleton (probably best done that way anyway), possibly moving clioize here, and giving params to intitialize instead of having it read from Preference.instance.  Perhaps things like Password.lists and Password.dictionaries should be here as well.
+# This should have an instance variable that saves the plan. Once set, calling .pw will issue new passwords based on that plan.
 class Password
   attr_reader :pw
 
-  def self.components(options) # FIXME Unused, but returns class parts needed by settings.  Implement instatation based on this?  Would move Punctuation and Number to files.  Other impact/changes...  Maybe load all classes, but keep @list separate from the class, and add @list via metaprogramming only if needed, by finding a matching .yml file.  This would allow for some smart part classes.  They could check and see if there is a file match, if so add list, if not get list some other way (ie: dynamic lists like pig-latin, truly random secrets) Ie: Part.descendants.each{|d| d.add_list}, and add_list checks for files, on fail does something else (for dynamic lists).  Allows treating all lists much more uniformly and better encapsulated.
+  def self.components(options) # FIXME Unused, but returns class parts needed by settings.  Implement instantiation based on this?  Would move Punctuation and Number to files.  Other impact/changes...  Maybe load all classes, but keep @list separate from the class, and add @list via metaprogramming only if needed, by finding a matching .yml file.  This would allow for some smart part classes.  They could check and see if there is a file match, if so add list, if not get list some other way (ie: dynamic lists like pig-latin, truly random secrets) Ie: Part.descendants.each{|d| d.add_list}, and add_list checks for files, on fail does something else (for dynamic lists).  Allows treating all lists much more uniformly and better encapsulated.
     parts = []
     options.plan.each do |part|
       part[0] == 'Word' ? parts << options.config[:dictionary] : parts << part[0]
