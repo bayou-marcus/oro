b6ce567a10e52e30032ebf7b951142cf7de1bad2
Joel Wagener <jwagener@aibs.org>
2016-02-27 04:28:50 -0500

    Updated version, again.

--- lib/auguste/settings_parser.rb
+++ lib/auguste/settings_parser.rb
@@ -9,7 +9,7 @@ class SettingsParser
     clio_parser = OptionParser.new do |clio_parser_config|
       clio_parser_config.program_name = 'Auguste'
       clio_parser_config.release = 'Command Line Version'
-      clio_parser_config.version = %w(1 0 beta 0)
+      clio_parser_config.version = %w(1 0 0)
       clio_parser_config.banner = 'Usage: auguste [settings]'
       clio_parser_config.separator ''
 
